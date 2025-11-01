6dd3d86b2f720b7705253f2e746d68063c75ce4e
forasgarddd <forbattlenetonly14@gmail.com>
2025-11-01 22:17:18 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ivan Devitskyi on 01/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
