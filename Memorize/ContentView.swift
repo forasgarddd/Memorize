b7e56e6787742e1b8f28db5774d4a34d8fcdd7aa
forasgarddd <forbattlenetonly14@gmail.com>
2025-11-07 16:58:09 +0100

    Added 3 themes

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,55 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["🫨", "🤐", "🤩", "🤯", "🙃", "😎", "😡", "🫠", "🥶", "🤢", "😈", "🤡"]
-    @State var cardCount = 4
+
+    @State var currentTheme : Theme
+    @State var cardCount : Int
     
+    let themes = [
+        Theme(name: "Activities", emojis: ["⚽️", "🏀", "⚽️", "🏀", "🎱", "🎾", "🎱", "🎾", "🥏", "🏓", "🥏", "🏓", "🪀", "🎯", "🪀", "🎯", "🥊", "🥊"], symbol: "sportscourt", color: .green),
+        Theme(name: "Animals", emojis: ["🐶", "🐱", "🐶", "🐱", "🐰", "🦊", "🐰", "🦊", "🐷", "🐷"], symbol: "pawprint", color: .blue),
+        Theme(name: "Faces", emojis: ["🫨", "🤐", "🫨", "🤐", "🙃", "😎", "🙃", "😎", "🥶", "🤢", "🥶", "🤢"], symbol: "face.smiling", color: .red)
+    ]
+    
+    init() {
+        self.cardCount = themes[0].emojis.count
+        self.currentTheme = themes[0]
+    }
+
     var body: some View {
         VStack {
+            Text("Memorize")
+                .font(.largeTitle)
             ScrollView {
                 cards
             }
             Spacer()
-            cardCountAdjusters
+            themeChangers
         }
         .padding()
     }
+
     
-    var cardCountAdjusters: some View {
+    var themeChangers: some View {
         HStack {
-            cardAdder
-            Spacer()
-            cardRemover
+            ForEach(themes, id: \.name) { theme in
+                Button(action: {
+                    changeTheme(to: theme)
+                }, label: {
+                    VStack {
+                        Image(systemName: theme.symbol)
+                            .imageScale(.large)
+                        Text(theme.name)
+                            .font(.subheadline)
+                    }
+                })
+            }
         }
-        .imageScale(.large)
-        .font(.largeTitle)
     }
     
     var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 90))]) {
-            ForEach(0..<cardCount, id: \.self) { index in
-                CardView(content: emojis[index])
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 75))]) {
+            ForEach(0..<currentTheme.emojis.count, id: \.self) { index in
+                CardView(content: currentTheme.emojis[index])
                     .aspectRatio(2/3, contentMode: .fit)
             }
         }
-        .foregroundStyle(.red)
+        .foregroundStyle(currentTheme.color)
+    }
+
+    
+    func changeTheme(to theme: Theme) {
+        currentTheme.emojis = theme.emojis.shuffled()
+        currentTheme.color = theme.color
+        if cardCount > currentTheme.emojis.count {
+            cardCount = currentTheme.emojis.count
+        } else {
+            cardCount = currentTheme.emojis.count
+        }
     }
     
-    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
-        Button(action: {
-            cardCount += offset
-        }, label: {
-            Image(systemName: symbol)
-        })
-        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
-    }
-    
-    var cardAdder: some View {
-        cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
-    }
-    
-    var cardRemover: some View {
-        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    func flipRandomPairOfCards() {
+        
     }
 }
 
@@ -70,7 +90,7 @@ struct CardView: View {
             let base = RoundedRectangle(cornerRadius: 12)
             Group {
                 base.fill(.white)
-                base.strokeBorder(lineWidth: 2)
+                base.strokeBorder(lineWidth: 4)
                 Text(content).font(.largeTitle)
             }
             .opacity(isFaceUp ? 1 : 0)
@@ -80,6 +100,13 @@ struct CardView: View {
             isFaceUp.toggle()
         }
     }
+}
+
+struct Theme {
+    let name: String
+    var emojis: [String]
+    let symbol: String
+    var color: Color
 }
 
 #Preview {
